ecfccfb4e02924d0b4cf1305eb6cd09899bcf8d4
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-09-16 21:25:17 +0900

    added validations when sending invitation

--- app/api/invitation/route.ts
+++ app/api/invitation/route.ts
@@ -9,9 +9,13 @@ export async function POST(req: NextRequest) {
     const user = await getUserFromSession();
     const userId = user!.id!;
     const data: InvitationFormSchema = await req.json();
-    const userToInvite = await prisma.user.findUnique({
-      where: { email: data.inviteeEmail },
-    });
+    const [userToInvite, invitation, group] = await Promise.all([
+      await prisma.user.findUnique({ where: { email: data.inviteeEmail } }),
+      await prisma.invitation.findFirst({
+        where: { groupId: data.groupId, inviteeEmail: data.inviteeEmail, status: 'PENDING' },
+      }),
+      await prisma.group.findUnique({ where: { id: data.groupId }, include: { members: true } }),
+    ]);
 
     if (!userToInvite) {
       return handleFail({
@@ -19,6 +23,21 @@ export async function POST(req: NextRequest) {
         message: '존재하지 않는 사용자입니다',
       });
     }
+
+    if (invitation) {
+      return handleFail({
+        status: HttpStatusCode.BadRequest,
+        message: '이미 초대를 발송한 사용자입니다',
+      });
+    }
+
+    if (group?.members.map((member) => member.userId).includes(userToInvite.id)) {
+      return handleFail({
+        status: HttpStatusCode.BadRequest,
+        message: '이미 그룹에 속한 사용자입니다',
+      });
+    }
+
     const createdInvitation = await prisma.invitation.create({
       data: {
         inviteeEmail: data.inviteeEmail,
