0b39c11495cb90b91f20b484a47d88ededa64624
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-10-16 18:45:43 +0900

    block group creation

--- components/pages/GroupList/HeaderButtons.tsx
+++ components/pages/GroupList/HeaderButtons.tsx
@@ -3,12 +3,12 @@
 import { Button } from '@/components/ui/button';
 import { PATHS } from '@/const/paths';
 import { useCurrentUrl } from '@/hooks/useCurrentUrl';
-// import { useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-// import { GROUP_CREATION_ALLOWED_USERS } from './const';
+import { GROUP_CREATION_ALLOWED_USERS } from './const';
 
 export default function HeaderButtons() {
-  // const session = useSession();
+  const session = useSession();
   const router = useRouter();
   const currentUrl = useCurrentUrl();
 
@@ -17,15 +17,12 @@ export default function HeaderButtons() {
 
   return (
     <div className="flex items-center gap-2">
-      <Button type="button" onClick={moveToCreateGroup}>
-        그룹 생성
-      </Button>
-      {/* {!!session?.data?.user?.email &&
+      {!!session?.data?.user?.email &&
         GROUP_CREATION_ALLOWED_USERS.includes(session?.data?.user?.email) && (
           <Button type="button" onClick={moveToCreateGroup}>
             그룹 생성
           </Button>
-        )} */}
+        )}
     </div>
   );
 }
