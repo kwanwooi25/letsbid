c0af72e07007086e323be5f9110cdd4f34972d7b
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-10-23 18:05:49 +0900

    added skeleton for auction detail page

---
+++ app/group/[groupId]/auction-case/components/AuctionCase/skeleton.tsx
@@ -0,0 +1,18 @@
+import PageBody from '@/components/PageBody';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function AuctionCaseSkeleton() {
+  return (
+    <>
+      <Skeleton.PageHeader className="max-w-2xl" backButton actionButtonCount={2} />
+      <PageBody className="max-w-2xl flex flex-col gap-6">
+        <div className="flex flex-col gap-4 md:flex-row md:items-center justify-between min-h-[28px]">
+          <div className="flex items-center gap-3">
+            <Skeleton className="h-[28px] w-[80px] rounded-full" />
+            <Skeleton className="h-[20px] w-[170px]" />
+          </div>
+        </div>
+      </PageBody>
+    </>
+  );
+}
