3e83dd09010800d2ed3a88d9e59ca9f0934cd142
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-11-06 21:59:55 +0900

    added mobile field to user form

--- components/UserImage/index.tsx
+++ components/UserImage/index.tsx
@@ -1,10 +1,12 @@
+import { cn } from '@/lib/utils';
 import { LucideUser2 } from 'lucide-react';
 import Image from 'next/image';
+import { HTMLAttributes } from 'react';
 
-export default function UserImage({ src, alt = 'user image', size = 64 }: Props) {
+export default function UserImage({ className, src, alt = 'user image', size = 64 }: Props) {
   return !!src ? (
     <Image
-      className="rounded-full object-cover"
+      className={cn('rounded-full object-cover', className)}
       style={{
         minWidth: `${size}px`,
         minHeight: `${size}px`,
@@ -18,7 +20,10 @@ export default function UserImage({ src, alt = 'user image', size = 64 }: Props)
     />
   ) : (
     <div
-      className="flex items-center justify-center rounded-full bg-primary-foreground"
+      className={cn(
+        'flex items-center justify-center rounded-full bg-primary-foreground',
+        className,
+      )}
       style={{ width: size, height: size }}
     >
       <LucideUser2 className="w-[60%] h-[60%]" />
@@ -27,6 +32,7 @@ export default function UserImage({ src, alt = 'user image', size = 64 }: Props)
 }
 
 type Props = {
+  className?: HTMLAttributes<HTMLDivElement>['className'];
   src?: string | null;
   alt?: string;
   size?: number;
