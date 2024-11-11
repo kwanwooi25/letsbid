ee74638288e3098473d8f83850f3e53f2879538d
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-11-11 22:13:05 +0900

    auth pages

--- queries/user/mutation.ts
+++ queries/user/mutation.ts
@@ -1,5 +1,5 @@
-import { SignInFormSchema } from '@/app/auth/sign-in/components/SignInForm/formSchema';
-import { SignUpFormSchema } from '@/app/auth/sign-up/components/SignUpForm/formSchema';
+import { SignInFormSchema } from '@/components/pages/SignInForm/formSchema';
+import { SignUpFormSchema } from '@/components/pages/SignUpForm/formSchema';
 import { UserFormSchema } from '@/components/pages/UserForm/formSchema';
 import { API_ROUTE } from '@/const/paths';
 import { SuccessResponse } from '@/types/api';
