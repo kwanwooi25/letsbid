44b901e9202738c71564624bff3f4a9aba8a701c
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-11-13 15:16:44 +0900

    added helper text for bidding form

---
+++ const/number.ts
@@ -0,0 +1,5 @@
+export const numberInKorean = ['', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
+
+export const numberUnitBelowThousandsInKorean = ['', '십', '백', '천'];
+
+export const numberUnitAboveThousandsInKorean = ['', '만', '억', '조', '경'];
--- lib/number.ts
+++ lib/number.ts
@@ -1,3 +1,10 @@
+import {
+  numberInKorean,
+  numberUnitAboveThousandsInKorean,
+  numberUnitBelowThousandsInKorean,
+} from '@/const/number';
+import chunk from 'lodash/chunk';
+
 export function squareMeterToPY(sqm?: number | string | null, decimal: number = 2) {
   if (!sqm) return null;
 
@@ -10,4 +17,25 @@ export function isValidNumber(input: unknown, includeNegative?: boolean) {
   }
 
   return typeof input === 'number' && input > 0;
+}
+
+export function readNumberInKorean(input: number | string) {
+  const inputString = String(input);
+  if (inputString.length > 20) return '';
+
+  const inputArray = chunk(inputString.split('').reverse(), 4);
+
+  const translatedInputs = inputArray.map((chunk, i) => {
+    const chunkInKorean = chunk.map((digit, j) => {
+      if (numberInKorean[+digit]) {
+        return `${numberInKorean[+digit]}${numberUnitBelowThousandsInKorean[j]}`;
+      }
+      return '';
+    });
+    if (chunkInKorean.some((d) => d)) {
+      return `${chunkInKorean.reverse().join('')}${numberUnitAboveThousandsInKorean[i]}`;
+    }
+  });
+
+  return translatedInputs.reverse().join(' ');
 }
