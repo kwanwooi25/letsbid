4dc83cea324bf2f777eb2c30403dbc2076bbaeb1
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-11-26 21:16:37 +0900

    added metadata for each page

--- app/group/[groupId]/auction-case/[auctionCaseId]/edit/page.tsx
+++ app/group/[groupId]/auction-case/[auctionCaseId]/edit/page.tsx
@@ -1,7 +1,35 @@
 import AuctionCaseForm from '@/components/pages/AuctionCaseForm';
 import { getAuctionCaseDetailQueryOptions } from '@/features/auction-case/query';
+import { getFullAddress } from '@/features/auction-case/utils';
 import { withAuth } from '@/features/auth/withAuth';
+import { getGroupDetailQueryOptions } from '@/features/group/query';
+import { getAppName } from '@/lib/env';
 import { getQueryClient } from '@/lib/query';
+import { Metadata } from 'next';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { groupId: string; auctionCaseId: string };
+}): Promise<Metadata> {
+  const queryClient = getQueryClient();
+  const [group, auctionCase] = await Promise.all([
+    queryClient.fetchQuery(getGroupDetailQueryOptions(params.groupId)),
+    queryClient.fetchQuery(getAuctionCaseDetailQueryOptions(params.auctionCaseId)),
+  ]);
+
+  if (!auctionCase) {
+    return {
+      title: `경매 사건 수정 | ${group.name} | ${getAppName()}`,
+      description: group.description,
+    };
+  }
+
+  return {
+    title: `경매 사건 수정 | ${auctionCase.caseName} | ${group.name} | ${getAppName()}`,
+    description: getFullAddress(auctionCase),
+  };
+}
 
 export default withAuth(async function ({
   params: { groupId, auctionCaseId },
